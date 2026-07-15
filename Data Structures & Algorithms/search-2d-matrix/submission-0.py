9adb0413ed23caf19af6f8f802c177acf4f56c5c
Sanket Gaikwad <136094323+Sanket-g-75@users.noreply.github.com>
2026-07-15 18:13:36 +0530

    Add: search-2d-matrix - submission-0

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # In this can' I directly flatten the list 
+
+        nums = None
+        for  i in matrix:
+            if nums is None:
+                nums =  i
+            else:
+                nums.extend(i)
+
+        l = 0
+        r = len(nums)-1
+
+        while l <=r:
+            mid =  (l+r)//2
+
+            if nums[mid] == target:
+                return True
+            elif target < nums[mid]:
+                r = mid -1
+            elif target > nums[mid]:
+                l = mid + 1
+        return False
+            
+            

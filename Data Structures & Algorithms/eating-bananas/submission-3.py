6097be8a2f1f49c42a8adc0d9b979ab39ee3a1c4
Sanket Gaikwad <136094323+Sanket-g-75@users.noreply.github.com>
2026-07-16 08:51:48 +0530

    Add: eating-bananas - submission-3

---
+++ Data Structures & Algorithms/eating-bananas/submission-3.py
@@ -0,0 +1,47 @@
+class Solution:
+
+    def ttaken(self,lst,h):
+
+        counter = 0
+
+        for i in lst:
+            
+            if  i%h == 0:
+                counter += int(i/h)
+            else:
+                counter += int(i/h) + 1
+        
+        return counter
+
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # So I have to find the minimum no. of bananas/hours so that the eating completes inside the timing
+
+        '''
+        So first I will have to find out what can be the max. k
+        So as this is a list, max is not known as lists have no metadata, so first I will have to find the max
+        for which I will have to use the binary search
+        '''
+        l = 1
+        r = max(piles)
+        result = 1000000000
+        
+        while l <= r:
+            
+            m = (l + r)//2
+            tm = self.ttaken(piles,m)
+
+            if tm > h:
+                l = m +1
+            elif tm <= h:
+                result = min(result,m)
+                r = m-1
+        return result
+            
+
+        
+
+            
+
+
+
+        
